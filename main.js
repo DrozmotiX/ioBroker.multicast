f189530b0a8b555a903410c39134c5b691620e83
Dutchman <rdrozda86@gmail.com>
2019-03-31 22:03:10 +0200

    version 0.0.6 beta release candidate

--- main.js
+++ main.js
@@ -12,11 +12,8 @@ const utils = require("@iobroker/adapter-core");
 const dgram = require("dgram");
 
 // Here Global variables
-let receive_ip;
-let receive_port;
-let send_ip;
-let send_port;
-let multicast;		 
+let receive_ip, receive_port, send_ip, send_port, multicast, timeout;
+const send_Delay = 200;
 
 class Multicast extends utils.Adapter {
 
@@ -38,7 +35,7 @@ class Multicast extends utils.Adapter {
 	/**
 	 * Is called when databases are connected and adapter received configuration.
 	 */
-	async onReady() {
+	onReady() {
 		// Init your Adapter here
 		receive_ip = this.config.receive_1 + "." + this.config.receive_2 + "." + this.config.receive_3 + "." + this.config.receive_4;
 		receive_port = this.config.receive_port;
@@ -63,6 +60,10 @@ class Multicast extends utils.Adapter {
 			this.log.info("Multicast Client listening on " + receiveaddress.address + ":" + receiveaddress.port + "and send to " + send_ip + ":" + send_port);
 		});
 
+		// Bind UDP listener to receice port
+		multicast.bind(receive_port);
+		this.subscribeStates("*");
+		// this.doTimesync();
 
 		// Ack on message in multicast listener
 		multicast.on("message", (message, remote) => {
@@ -76,153 +77,26 @@ class Multicast extends utils.Adapter {
 			try {
 				//@ts-ignore message is available from function
 				const received_data = JSON.parse(message);	// Versuch den String in ein JSON zu verwandeln
-				const statename = received_data.i["Type"] + "_" + received_data.i["MAC_Adress"];
+				const statename = received_data.i["Devicename"];
+				// const statename = received_data.i["Type"] + "_" + received_data.i["MAC_Adress"];
 				
-				this.log.info("Data from device " + statename + " correctly received");
-				this.setObjectNotExists(statename,{
-					type: "device",
-					common: {
-						name : received_data.c["Hostname"].v
-					},
-					native: {},
-				});
 
-				// Read all device related information and write into object with extend objection function
-				const objects = Object.keys(received_data.i);
-				let array = [];
-				for (const i in objects){
-					// Prepare values to be extended in instance object
-					array.push('"' + objects[i] + '" : "' + received_data.i[objects[i]] + '"');
+				// read array if message is related to initialisation or state update
+				if (received_data.Type == "Object") {
+					// initialization
+					this.log.debug("Data from device " + JSON.stringify(received_data));
+					this.log.info("Data for device  " + statename + " received, initializing device");
+					this.initialize(received_data, statename);
+				
+				} else if (received_data.Type == "State") {
+					// state update
+					this.stateupdate(received_data, true);
 
-					// Create state in info channel
-					this.createChannel(statename, "info");
-
-					const writable = false;
-
-					this.log.debug('"' + objects[i] + '" : "' + received_data.i[objects[i]] + '"');
-
-					// Check if state must contain min and max value
-					let min = "";
-					let max = "";
-					if (received_data.i[objects[i]].min !== undefined){
-						min = received_data.i[objects[i]].min;
-					}
-					
-					if (received_data.i[objects[i]].max !== undefined){
-						max = received_data.i[objects[i]].max;
-					}
-
-					// tdoStateCreate(device, name, type,role, unit, write)
-					this.doStateCreate(statename + ".Info." + objects[i], objects[i] , "number", received_data.i[objects[i]].r,"", writable, min, max);
-					this.setState(statename + ".Info." + objects[i], { val: received_data.i[objects[i]],ack: true});
-
-				}				
-
-				// update name
-				array.push('"' + "name" + '" : "' + received_data.c["Hostname"].v + '"');
-
-				// Write attributes to instance object
-				array = JSON.parse("{" + array + "}"); //Finalize creation of object
-				const objekt = {};
-				objekt.common = array;
-				this.extendObject(statename, objekt, (err) => {
-					if (err !== null){this.log.error("Changing alias name failed with : " + err);}
-				});	
-		
-				// Read all configuration related information and write into object with extend objection function
-
-				// To-Do :  Implement cache for case JSON does not contain configuration data
-				const config = Object.keys(received_data.c);
-				for (const i in config){
-					const writable = true;
-
-					// Create state in info channel
-					this.createChannel(statename, "Config");
-
-					// Define if state must be part of channel and create state name
-					let stateCreateName = statename + ".Config." + config[i];
-					this.log.debug("stateCreateName before if : " + stateCreateName);
-					if (received_data.c[config[i]].c === undefined){
-						// do nothing
-					}
-					else { 
-						stateCreateName = statename + ".Config." + received_data.c[config[i]].c + "." + config[i];
-					}
-					this.log.debug("stateCreateName after if : " + stateCreateName);
-					
-					// Check if state must contain min and max value
-					let min = "";
-					let max = "";
-					if (received_data.c[config[i]].min !== undefined){
-						min = received_data.c[config[i]].min;
-					}
-					
-					if (received_data.c[config[i]].max !== undefined){
-						max = received_data.c[config[i]].max;
-					}
-
-					this.log.debug('"' + config[i] + '" : "' + received_data.c[config[i]] + '"');
-
-					// tdoStateCreate(device, name, type,role, unit, write)
-					this.doStateCreate(stateCreateName, config[i] , received_data.c[config[i]].t, received_data.c[config[i]].r,received_data.c[config[i]].u, writable, min, max);
-					this.setState(stateCreateName, { val: received_data.c[config[i]].v,ack: true});
-
-					// if state has writable flag yes, subscribe on changes
-					if (writable === true) {
-						this.subscribeStates(stateCreateName);
-						this.log.debug("State subscribed!: "+ stateCreateName);
-					}
-
-				}						
-
-				// Create states for all values received in JSON
-
-				// To-Do :  Implement cache for case JSON does not contain state data
-
-				const stateNames = Object.keys(received_data.s);
-				for (const i in stateNames){
-					let writable = false;
-					if (received_data.s[stateNames[i]].w === undefined){
-						writable =	false;
-					}
-					else if (received_data.s[stateNames[i]].w === true || received_data.s[stateNames[i]].w === "true") { 
-						writable =	true;
-					}
-
-					// Define if state must be part of channel and create state name
-					let stateCreateName = statename + "." + stateNames[i];
-					this.log.debug("stateCreateName before if : " + stateCreateName);
-					if (received_data.s[stateNames[i]].c === undefined){
-						// do nothing
-					}
-					else { 
-						stateCreateName = statename + "." + received_data.s[stateNames[i]].c + "." + stateNames[i];
-					}
-					this.log.debug("stateCreateName after if : " + stateCreateName);
-
-					// Check if state must contain min and max value
-					let min = "";
-					let max = "";
-					if (received_data.s[stateNames[i]].min !== undefined){
-						min = received_data.s[stateNames[i]].min;
-					}
-					
-					if (received_data.s[stateNames[i]].max !== undefined){
-						max = received_data.s[stateNames[i]].max;
-					}
-
-					// create state
-					this.doStateCreate(stateCreateName, stateNames[i], received_data.s[stateNames[i]].t, received_data.s[stateNames[i]].r,received_data.s[stateNames[i]].u, writable, min, max);
-					
-					// write value into state
-					this.setState(stateCreateName, { val: received_data.s[stateNames[i]].v,ack: true, expire: (received_data["i"].interval *3) + 2 });
-					
-					// if state has writable flag yes, subscribe on changes
-					if (writable === true) {
-						this.subscribeStates(stateCreateName);
-						this.log.debug("State subscribed!: "+ stateCreateName);
-					}
+				} else if (received_data.Type == "StateInterval") {
+					// state update
+					this.stateupdate(received_data, false);
 				}
+
 			} catch (error) {
 
 				//To-Do : ensure propper error logging for wrong firmware versions
@@ -230,8 +104,6 @@ class Multicast extends utils.Adapter {
 				this.log.error("unknown message received - please read the documentation !!!! - this was received: " + message);
 			}
 		});
-		multicast.bind(receive_port);
-		
 
 		/*
 		setState examples
@@ -259,7 +131,7 @@ class Multicast extends utils.Adapter {
 	 * Is called when adapter shuts down - callback has to be called under any circumstances!
 	 * @param {() => void} callback
 	 */
-	async onUnload(callback) {
+	onUnload(callback) {
 		try {
 			this.log.info("cleaned everything up...");
 			multicast.close();
@@ -300,10 +172,12 @@ class Multicast extends utils.Adapter {
 				for (let i=3; i <= deviceId.length-1; i++) {
 					stateNameToSend += deviceId[i];
 					if(i < (deviceId.length -1)) stateNameToSend += ".";
-					
-					this.log.warn("statename: "+stateNameToSend);
 				}
+
+				this.log.debug("statename: " + stateNameToSend);
+
 				const objekt = await this.getObjectAsync(deviceId[2]);
+				this.log.debug("Return of async getObject : " + JSON.stringify(objekt));
 				if (objekt !== undefined && objekt !== null) {
 					this.log.debug("AsyncObject received while StateChange: " + JSON.stringify(objekt));
 					const sendMessage = {
@@ -318,6 +192,190 @@ class Multicast extends utils.Adapter {
 			// The state was deleted
 			this.log.debug(`state ${id} deleted`);
 		}
+	}
+
+	initialize(received_data, statename){
+
+		// Create Device objekt
+		this.setObjectNotExists(statename,{
+			type: "device",
+			common: {
+				name : received_data.c["Hostname"].v
+			},
+			native: {},
+		});
+
+		// Read all device related information and write into object with extend objection function
+		const objects = Object.keys(received_data.i);
+		let array = [];
+		for (const i in objects){
+			// Prepare values to be extended in instance object
+			array.push('"' + objects[i] + '" : "' + received_data.i[objects[i]] + '"');
+
+			// Create state in info channel
+			this.createChannel(statename, "info");
+
+			// Info channel is always read only, set hardcoded
+			const writable = false;
+
+			this.log.debug('"' + objects[i] + '" : "' + received_data.i[objects[i]] + '"');
+
+			// Check if state must contain min and max value
+			const min = this.defineMin(received_data.i[objects[i]]);
+			const max = this.defineMax(received_data.i[objects[i]]);
+
+			// tdoStateCreate(device, name, type,role, unit, write)
+			this.doStateCreate(statename + ".Info." + objects[i], objects[i] , "number", received_data.i[objects[i]].r,"", writable, min, max);
+			this.setState(statename + ".Info." + objects[i], { val: received_data.i[objects[i]],ack: true});
+
+		}
+
+		// update name
+		array.push('"' + "name" + '" : "' + received_data.c["Hostname"].v + '"');
+
+		// Write attributes to device object
+		array = JSON.parse("{" + array + "}"); //Finalize creation of object
+		const objekt = {};
+		objekt.common = array;
+
+		timeout = setTimeout( () => {
+		// message here !
+			this.extendObject(statename, objekt, (err) => {
+				if (err !== null){this.log.error("Changing alias name failed with : " + err);}
+			});
+		}, 2000);
+		
+		
+		
+		// Read all configuration related information and write into object with extend objection function
+		// To-Do :  Implement cache for case JSON does not contain configuration data
+		const config = Object.keys(received_data.c);
+		for (const i in config){
+			// Config channel is always writable, set hardcoded
+			const writable = true;
+
+			// Create state in info channel
+			this.createChannel(statename, "Config");
+
+			// Define if state must be part of channel and create state name
+			let stateCreateName = statename + ".Config." + config[i];
+			this.log.debug("stateCreateName before if : " + stateCreateName);
+			if (received_data.c[config[i]].c === undefined){
+				// do nothing
+			}
+			else { 
+				stateCreateName = statename + ".Config." + received_data.c[config[i]].c + "." + config[i];
+			}
+			this.log.debug("stateCreateName after if : " + stateCreateName);
+			
+			// Check if state must contain min and max value
+			const min = this.defineMin(received_data.c[config[i]]);
+			const max = this.defineMax(received_data.c[config[i]]);
+
+			this.log.debug('"' + config[i] + '" : "' + received_data.c[config[i]] + '"');
+
+			// create state and update value 
+			this.doStateCreate(stateCreateName, config[i] , received_data.c[config[i]].t, received_data.c[config[i]].r,received_data.c[config[i]].u, writable, min, max);
+			this.setState(stateCreateName, { val: received_data.c[config[i]].v,ack: true});
+
+			// if state has writable flag yes, subscribe on changes
+			this.dosubscribe(stateCreateName, writable);
+
+		}						
+
+		// Create states for all values received in JSON
+		// To-Do :  Implement cache for case JSON does not contain state data
+
+		const stateNames = Object.keys(received_data.s);
+		for (const i in stateNames){
+			const writable = this.defineWritable(received_data.s[stateNames[i]]);
+
+			// Define if state must be part of channel and create state name
+			let stateCreateName = statename + "." + stateNames[i];
+			this.log.debug("stateCreateName before if : " + stateCreateName);
+			if (received_data.s[stateNames[i]].c === undefined){
+				// do nothing
+			}
+			else { 
+				stateCreateName = statename + "." + received_data.s[stateNames[i]].c + "." + stateNames[i];
+			}
+			this.log.debug("stateCreateName after if : " + stateCreateName);
+
+			// Check if state must contain min and max value
+			const min = this.defineMin(received_data.s[stateNames[i]]);
+			const max = this.defineMax(received_data.s[stateNames[i]]);
+
+			// create state and update value 
+			this.doStateCreate(stateCreateName, stateNames[i], received_data.s[stateNames[i]].t, received_data.s[stateNames[i]].r,received_data.s[stateNames[i]].u, writable, min, max);
+			
+			this.log.debug("Value of intervall : " + received_data.c["Interval_Object"].v);
+			this.setState(stateCreateName, { val: received_data.s[stateNames[i]].v,ack: true, expire: (received_data.c["Interval_Object"].v *3) + 2 });
+			
+			// if state has writable flag yes, subscribe on changes
+			this.dosubscribe(stateCreateName, writable);
+		}
+
+		const confirm = {
+			i : objekt.common,
+			Type : "synced"				
+		};
+		// message here !
+		this.log.info("Device " + statename + " successfully initiated");
+		this.transmit(JSON.stringify(confirm));
+
+	}
+
+	// function to handle state updates
+	async stateupdate(received_data, confirm){
+		const stateNames = Object.keys(received_data.s);
+		const exp_timer = await this.getStateAsync(this.namespace + ".Config.System.Interval_Measure");
+		this.log.debug("Configured interval : " + this.namespace + " || " + exp_timer);
+
+		for (const i in stateNames){
+
+			const statename = this.namespace + "." + stateNames[i];
+			this.log.debug("Statename to update : " + statename + " with value : " + received_data.s[stateNames[i]]);
+
+			const deviceId = statename.split(".");
+			let stateNameToSend = "";
+			for (let i=3; i <= deviceId.length-1; i++) {
+				stateNameToSend += deviceId[i];
+				if(i < (deviceId.length -1)) stateNameToSend += ".";
+			}			
+
+			this.log.debug("Statename to sent : " + deviceId[2]);
+
+			const objekt = await this.getObjectAsync(deviceId[2]);
+			this.log.debug("Return of async getObject : " + JSON.stringify(objekt));
+			if (objekt === undefined || objekt === null) {
+				const sendMessage = {
+					i : received_data.i,
+					Type : "sync"				
+				};
+				// message here !
+				this.transmit(JSON.stringify(sendMessage));
+			
+			} else {
+
+				// update state
+				// this.log.debug("Value of intervall : " + received_data.c["Interval"].v);
+				this.setState(statename, { val: received_data.s[stateNames[i]],ack: true});
+				
+				if (confirm === true){
+
+					const confirm = {
+						i : objekt.common,
+						Type : "OK"				
+					};
+
+					// timeout = setTimeout( () => {
+					// message here !
+					this.transmit(JSON.stringify(confirm));
+					// }, send_Delay);
+				}
+			}
+		}
+
 	}
 
 	// Function to handle state creation
@@ -340,11 +398,74 @@ class Multicast extends utils.Adapter {
 		});
 	}
 
+	defineMin (source) {
+		this.log.debug("Raw min function : " + JSON.stringify(source));
+		this.log.debug("Min : " + JSON.stringify(source.min));
+		let min = "";
+		if (source.min !== undefined){
+			min = source.min;
+		}
+		return min;
+
+	}
+
+	defineMax (source) {
+		this.log.debug("Raw max function : " + JSON.stringify(source));
+		this.log.debug("Min : " + JSON.stringify(source.max));
+		let max = "";
+		if (source.min !== undefined){
+			max = source.max;
+		}
+		return max;
+	}
+
+	defineWritable(source){
+		let writable = false;
+		if (source.w === undefined){
+			writable = false;
+		}
+		else if (source.w === true || source.w === "true") { 
+			writable =	true;
+		}
+		return writable;
+	}
+
+	dosubscribe (statename, writable){
+
+		if (writable === true) {
+			this.subscribeStates(statename);
+			this.log.debug("State subscribed!: "+ statename);
+		}
+
+	}
+
+	doTimesync(){
+		// In case of error, use 10 minutes as default
+		let intervall = 10000;
+		if (this.config.Time_Sync !== undefined && this.config.Time_Sync !== null){
+			intervall = (this.config.Time_Sync * 1000);
+		}
+		setInterval(() => {
+			const TimeStamp = (new Date().getTime());
+			const UnixTime = {
+				i : {
+					ts : TimeStamp
+				},
+				Type : "TimeSync"				
+			};
+			// message here !
+			this.transmit(JSON.stringify(UnixTime));
+
+		}, intervall);
+
+	}
+
 	// Send UDP message
 	transmit(message){
 		multicast.send(message, 0, message.length, parseInt(send_port) ,send_ip, (err, bytes) => {
 			if (err) throw err;
-			this.log.info("UDP Nachricht an IP " + send_ip + " an Port "+ parseInt(send_port) + " wurde versendet! - Die Länge der Nachricht war " + message.length +" Bytes");	
+			this.log.debug("UDP Message content : " + message);
+			this.log.debug("UDP Nachricht an IP " + send_ip + " an Port "+ parseInt(send_port) + " wurde versendet! - Die Länge der Nachricht war " + message.length +" Bytes");	
 		});
 	}
 }
--- test/unit.js
+++ test/unit.js
@@ -1,5 +1,18 @@
 const path = require("path");
 const { tests } = require("@iobroker/testing");
 
+const dgramMock = {
+	createSocket() {
+		return {
+			on() {}, // does nothing
+			bind() {}, // does nothing
+		};
+	}
+};
+
 // Run unit tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
-tests.unit(path.join(__dirname, ".."));
+tests.unit(path.join(__dirname, ".."), {
+	additionalMockedModules: {
+		dgram: dgramMock
+	}
+});
